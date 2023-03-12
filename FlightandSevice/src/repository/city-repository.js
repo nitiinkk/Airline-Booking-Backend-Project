58e56e3aec21cf8f1559f34647049d335b83d9fe
nitin2038 <acinonyx219@gmail.com>
2023-03-12 10:34:39 +0530

    completed city repository

--- FlightandSevice/src/repository/city-repository.js
+++ FlightandSevice/src/repository/city-repository.js
@@ -1,4 +1,30 @@
 //right way to interact with models
 const { City } = require('../models/index');
 
-class CityRepository 
+class CityRepository {
+    async createCity ({name}) {
+        try {
+            const city = await City.create({name});
+            return city;
+        }
+        catch (error) {
+            throw {error};
+        }
+    }
+
+    async deleteCity (cityId) {
+        try {
+            await City.destroy({
+                where : {
+                    id : cityId
+                }            
+            })
+        }
+        catch (error) {
+            throw {error};
+        }
+    }
+    
+}
+
+module.exports = CityRepository;
