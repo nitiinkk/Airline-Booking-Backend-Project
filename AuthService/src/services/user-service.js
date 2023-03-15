99bb9a37c9f5af4e3bcc0898690bd107331727d2
nitink <nitin.kumar@ellucian.com>
2023-03-15 20:59:52 +0530

    added jwt verify and create token logic

--- AuthService/src/services/user-service.js
+++ AuthService/src/services/user-service.js
@@ -1,6 +1,8 @@
 //this Service layer has the buisness logic
+const jwt = require('jsonwebtoken');
 const UserRepository = require('../repository/user-repository');
 
+const { JWT_KEY } = require('../config/serverConfig');
 class UserService {
     constructor() {
         this.userRepository = new UserRepository();
@@ -32,6 +34,26 @@ class UserService {
             return user;
         } catch (error) {
             console.log(error);
+            throw {error};
+        }
+    }
+
+    createToken (user) {
+        try {
+            const result = jwt.sign(user, JWT_KEY, {expiresIn : '1h'});
+            return result;
+        } catch (error) {
+            console.log('Something went wrong in token creation');
+            throw {error};
+        }
+    }
+
+    verifyToken (token) {
+        try {
+            const response = jwt.verify(token, JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log('Something went wrong in token validation', error);
             throw {error};
         }
     }
