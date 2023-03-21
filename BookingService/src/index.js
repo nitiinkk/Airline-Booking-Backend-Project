6aff3e8cd9daf425219bce6ff7d9c94c56027105
nitink <nitin.kumar@ellucian.com>
2023-03-21 17:36:28 +0530

    setup Booking service

---
+++ BookingService/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { PORT } =  require('./config/serverConfig');
+
+const setupAndStartServer = () => {
+    const app = express();
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+
+
+    app.listen(PORT, ()=>{
+        console.log(`Server started on Port ${PORT}`);
+    })
+}
+
+setupAndStartServer();
