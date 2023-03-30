bd0b0758136e851c4bc2f988e770db378f3c5436
nitink <nitin.kumar@ellucian.com>
2023-03-30 16:36:52 +0530

    added migration and model files

--- ReminderService/Readme.md
+++ ReminderService/Readme.md
@@ -3,3 +3,16 @@
 ### Setup
 `We use nodemailer package to send mails`
 `1. Confiure App password for Email_Password for sending mails`
+`2. npx sequelize init`
+`3.config.json
+{
+  "development": {
+    "username": "root",
+    "password": "root@12345",
+    "database": "REMAINDER_DB_DEV",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
+`
+` 4. to create db : npx sequelize db:create`
---
+++ ReminderService/src/migrations/20230330110458-create-notification-ticket.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('NotificationTickets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      subject: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      recepientEmail: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.ENUM
+      },
+      notificationTime: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('NotificationTickets');
+  }
+};
